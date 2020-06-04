3c9899449e50557a3b34af4988ee322ced95a20a
Praful-a <prafuldubey27@gmail.com>
2020-06-04 17:32:07 +0530

    Added Images to our Falshcard app.

--- FashCard/flashcard.py
+++ FashCard/flashcard.py
@@ -1,9 +1,10 @@
 from tkinter import *
-# from PIL import ImangeTK
+from PIL import ImageTk, Image
+from random import randint
 
 root = Tk()
 root.title("Learn To Code!")
-root.iconbitmap('favicon.ico')
+root.iconbitmap(r'C:\Users\Praful\Desktop\Tkinter\Tkinter\favicon.ico')
 root.geometry("500x500")
 
 # Create State FlashCard Function
@@ -13,7 +14,25 @@ def states():
     # hide previous frames
     hide_all_frames()
     state_frame.pack(fill="both", expand=1)
-    my_label = Label(state_frame, text="Something").pack()
+    # my_label = Label(state_frame, text="Something").pack()
+
+    # Create a list of state names
+    our_states = ['brazil', 'colombia', 'indonesia', 'philippines',
+                  'romania', 'saudi', 'taiwan', 'thailand', 'united-arab', 'vietnam']
+
+    # Generate a random number
+    rando = randint(0, len(our_states)-1)
+    state = 'Images/' + our_states[rando] + ".jpg"
+
+    # Create our state Images
+    global state_img
+    state_img = ImageTk.PhotoImage(Image.open(state))
+    show_state = Label(state_frame, image=state_img)
+    show_state.pack(pady=15)
+
+    # Create Button To Randomize State Images
+    rando_button = Button(state_frame, text="Next State", command=states)
+    rando_button.pack(pady=10)
 
 # Create State Capital FlashCard Function
 
