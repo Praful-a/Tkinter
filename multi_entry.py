1eab895be00bf1885ac874fa0e9b35f5232fdfca
Praful-a <prafuldubey27@gmail.com>
2020-06-06 23:21:18 +0530

    multiple entry boxes.

---
+++ multi_entry.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Entry Boxes")
+root.iconbitmap('favicon.ico')
+root.geometry("700x500")
+
+my_entries = []
+
+def something():
+    entry_list = ''
+    for entries in my_entries:
+        entry_list = entry_list + str(entries.get()) + '\n'
+        my_label.config(text=entry_list)
+
+    # print(my_entries[0].get())
+
+# Row loop
+for y in range(5):
+    # column loop
+    for x in range(5):
+        my_entry = Entry(root)
+        my_entry.grid(row=y, column=x, pady=20, padx=5)
+        my_entries.append(my_entry)
+
+my_button = Button(root, text="Click Me!", command=something)
+my_button.grid(row=6, column=0, pady=20)
+
+
+my_label = Label(root, text='')
+my_label.grid(row=7, column=0, pady=20)
+
+root.mainloop()
