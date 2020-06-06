1b086f8ed3fe78db3bd586625ed2ec5cdbe7fe2b
Praful-a <prafuldubey27@gmail.com>
2020-06-06 17:54:17 +0530

    Completed my math functionality in flashcard app.

--- FashCard/flashcard.py
+++ FashCard/flashcard.py
@@ -8,6 +8,88 @@ root.title("Learn To Code!")
 root.iconbitmap(r'C:\Users\Praful\Desktop\Tkinter\Tkinter\favicon.ico')
 root.geometry("500x650")
 
+# Create flashCard randomization
+def math_random():
+    # Generate a random number
+    global num_1
+    global num_2
+    num_1 = randint(0, 10)
+    num_2 = randint(0, 10)
+
+    global add_image1
+    global add_image2
+    card1 = 'Images/' + str(num_1) + ".png"
+    card2 = 'Images/' + str(num_2) + ".png"
+    add_image1 = ImageTk.PhotoImage(Image.open(card1))
+    add_image2 = ImageTk.PhotoImage(Image.open(card2))
+
+    # Put flashcard images on the screen
+    add_1.config(image=add_image1)
+    add_2.config(image=add_image2)
+
+
+# Create addition answer function
+def answer_add():
+    answer = num_1 + num_2
+    if int(add_answer.get()) == answer:
+        response = "Correct! " + str(num_1) + " + " + str(num_2) + " = " + str(answer)
+    else:
+        response = "Wrong! " + str(num_1) + " + " + str(num_2) + " = " + str(answer) + " Not " + add_answer.get()
+
+    answer_message.config(text=response)
+    add_answer.delete(0, 'end')
+    math_random()
+
+
+# Create Addition math flashCard Function
+def add():
+    hide_all_frames()
+    add_frame.pack(fill="both", expand=1)
+
+    add_label = Label(add_frame, text="Addition FlashCards", font=("Helvetica", 28)).pack(pady=15)
+
+    pic_frame = Frame(add_frame, width=400, height=300)
+    pic_frame.pack()
+
+    # Generate a random number
+    global num_1
+    global num_2
+    num_1 = randint(0, 10)
+    num_2 = randint(0, 10)
+
+    # Create 2 labels inside our pic frame, frame
+    global add_1
+    global add_2
+    add_1 = Label(pic_frame)
+    add_2 = Label(pic_frame)
+    math_sign = Label(pic_frame, text="+", font=("Helvetica", 34))
+    # Grid our labels
+    add_1.grid(row=0, column=0)
+    math_sign.grid(row=0, column=1)
+    add_2.grid(row=0, column=2)
+
+    global add_image1
+    global add_image2
+    card1 = 'Images/' + str(num_1) + ".png"
+    card2 = 'Images/' + str(num_2) + ".png"
+    add_image1 = ImageTk.PhotoImage(Image.open(card1))
+    add_image2 = ImageTk.PhotoImage(Image.open(card2))
+
+    # Put flashcard images on the screen
+    add_1.config(image=add_image1)
+    add_2.config(image=add_image2)
+
+    # Create answer box and button
+    global add_answer
+    add_answer = Entry(add_frame, font=("Helvetica", 18))
+    add_answer.pack(pady=40)
+
+    add_answer_button = Button(add_frame, text="Answer", command=answer_add)
+    add_answer_button.pack()
+
+    global answer_message
+    answer_message = Label(add_frame, text="", font=("Helvetica", 18))
+    answer_message.pack(pady=40)
 
 def random_state():
 
@@ -37,8 +119,6 @@ def state_capital_answer():
     answer_label_capitals.config(text=response)
 
 # Create answer function
-
-
 def state_answer():
     answer = answer_input.get()
     answer = answer.replace(" ", " ")
@@ -54,8 +134,6 @@ def state_answer():
     random_state()
 
 # Create State FlashCard Function
-
-
 def states():
     # hide previous frames
     hide_all_frames()
@@ -101,8 +179,6 @@ def states():
     answer_label.pack(pady=15)
 
 # Create State Capital FlashCard Function
-
-
 def state_capitals():
     # hide previous frames
     hide_all_frames()
@@ -183,14 +259,17 @@ def state_capitals():
     answer_label_capitals.pack(pady=15)
 
 # Hide all previous Frames
-
-
 def hide_all_frames():
     for widget in state_frame.winfo_children():
         widget.destroy()
 
     for widget in state_capitals_frame.winfo_children():
         widget.destroy()
+
+    for widget in add_frame.winfo_children():
+        widget.destroy()
+
+    add_frame.pack_forget()
     state_frame.pack_forget()
     state_capitals_frame.pack_forget()
 
@@ -207,8 +286,20 @@ states_menu.add_command(label="State capitals", command=state_capitals)
 states_menu.add_separator()
 states_menu.add_command(label="Exit", command=root.quit)
 
+# Math FlashCard Menu
+math_menu = Menu(my_menu)
+my_menu.add_cascade(label="Math", menu=math_menu)
+math_menu.add_command(label="Addition", command=add)
+
 # Create our Frames
 state_frame = Frame(root, width=500, height=500)
 state_capitals_frame = Frame(root, width=500, height=500)
+
+# create our Frames
+state_frame = Frame(root, width=500, height=500)
+state_capitals_frame = Frame(root, width=500, height=500)
+add_frame = Frame(root, width=500, height=500)
+
+
 
 root.mainloop()
