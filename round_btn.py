9bb856f14c76a8c9133e73c2c02fcb52266cec11
Praful-a <prafuldubey27@gmail.com>
2020-06-07 14:45:17 +0530

    Added button in tkinter.

---
+++ round_btn.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = Tk()
+root.title("Entry Boxes")
+root.iconbitmap('favicon.ico')
+root.geometry("400x400")
+
+def thing():
+    my_label.config(text="You clicked the button...")
+
+image = Image.open('round.jpg')
+login_btn = ImageTk.PhotoImage(image)
+
+img_label = Label(image=login_btn)
+# img_label.pack(pady=20)
+
+my_button = Button(root, image=login_btn, command=thing, borderwidth=0)
+my_button.pack(pady=20)
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
+
+
+root.mainloop()
