bd4f3bc155368a3539c47910a17204da5e22bd3e
Praful-a <prafuldubey27@gmail.com>
2020-06-07 16:07:48 +0530

    using tkCalendar

---
+++ calender.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkcalendar import *
+
+root = Tk()
+root.title('Move objects by Aerow key')
+root.iconbitmap('favicon.ico')
+root.geometry("600x400")
+
+cal = Calendar(root, selectmode="day", year=2020, month=5, day=22)
+cal.pack(pady=20, fill="both", expand=True)
+
+
+def grab_date():
+    my_label.config(text="Today's Date Is " + cal.get_date())
+
+my_button = Button(root, text="Get Date", command=grab_date)
+my_button.pack(pady=20)
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
+
+root.mainloop()
